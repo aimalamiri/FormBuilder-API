ddf4ee305d2840a4e20c821c6c4216f90b6a088b
Aimal Amiri <meaimal@hotmail.com>
2022-11-03 20:25:46 +0430

    Add test for project name validation

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe Project, type: :model do
     subject { Project.create!(name: "Test Project", description: "This is a test project.", user_id: 1) }
     before { subject.save }
 
-    it "is invalid with empty name" do
+    it "should have a name" do
       subject.name = nil
       expect(subject).to_not be_valid(:name)
+    end
+
+    it "should have a description" do
+      subject.description = nil
+      expect(subject).to_not be_valid(:description)
     end
   end
 end
