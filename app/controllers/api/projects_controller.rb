eaef07832548286d54f18642472daee3ca28e126
Aimal Amiri <meaimal@hotmail.com>
2022-11-03 18:52:28 +0430

    Add create, update, and delete api handlers for projects

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -3,4 +3,37 @@ class Api::ProjectsController < ApplicationController
     projects = Project.all
     render :json => projects
   end
+
+  def show
+    project = Project.find(params[:id])
+    render :json => project
+  end
+
+  def create
+    project = Project.create(project_params)
+    render :json => project
+  end
+
+  def edit
+    project = Project.find(params[:id])
+    render :json => project
+  end
+
+  def update
+    project = Project.find(params[:id])
+    project.update(project_params)
+    render :json => project
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    project.destroy
+    render :json => project
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :user_id)
+  end
 end
