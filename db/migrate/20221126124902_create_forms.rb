370ee8bce6151e83bbf00f480d37bf43cc31765e
Aimal Amiri <meaimal@hotmail.com>
2022-11-26 17:27:10 +0430

    Add necesseary columsn to forms_table

--- db/migrate/20221126124902_create_forms.rb
+++ db/migrate/20221126124902_create_forms.rb
@@ -1,6 +1,10 @@
 class CreateForms < ActiveRecord::Migration[7.0]
   def change
     create_table :forms do |t|
+      t.belongs_to :user
+      t.string :name
+      t.string :description
+      t.json :fields
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_181945) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_124902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "forms", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "description"
+    t.json "fields"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_forms_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
