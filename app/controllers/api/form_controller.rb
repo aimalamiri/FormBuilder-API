bb4e5f9a795ac45eb882b6a56ff59cd067efb60a
Aimal Amiri <meaimal@hotmail.com>
2022-11-26 17:43:31 +0430

    Create FromController with index and create actions

---
+++ app/controllers/api/form_controller.rb
@@ -0,0 +1,19 @@
+class Api::FormController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    forms = Form.all
+    render json: forms
+  end
+
+  def store
+    form = current_user.forms.create(form_params)
+    render json: form
+  end
+
+  private
+
+  def form_params
+    params.require(:form).permit(:name, :description, :fields)
+  end
+end
