9fe8de7d9b98dd81cd90a3e15579ca56d4c77ae3
Aimal Amiri <meaimal@hotmail.com>
2022-12-04 23:02:09 +0430

    Add forms relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,28 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :projects
+  has_many :forms
 
-  validates :first_name, presence: { message: 'First name field is empty.' }, length: { maximum: 30 }
-  validates :last_name, presence: { message: 'Last name field is empty.' }, length: { maximum: 30 }
-  validates :email, uniqueness: { message: 'This email is already registered.' }, format: {
-    with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
-    message: 'Email invalid'
-  }
+  validates :first_name,
+            presence: {
+              message: "First name field is empty."
+            },
+            length: {
+              maximum: 30
+            }
+  validates :last_name,
+            presence: {
+              message: "Last name field is empty."
+            },
+            length: {
+              maximum: 30
+            }
+  validates :email,
+            uniqueness: {
+              message: "This email is already registered."
+            },
+            format: {
+              with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/,
+              message: "Email invalid"
+            }
 end
