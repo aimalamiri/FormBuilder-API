d56fe9c16982b787bfd75c581b1892428c056001
Aimal Amiri <meaimal@hotmail.com>
2022-12-11 18:09:45 +0430

    Add entries/form route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # Authentication routes
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
-  get "/authorized", to: "sessions#show"
-  get "/dashboard", to: "users#show"
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  get '/authorized', to: 'sessions#show'
+  get '/dashboard', to: 'users#show'
 
   # Projects
-  namespace "api" do
+  namespace 'api' do
     resources :projects
     resources :forms, only: %i[index create edit update destroy]
+    get '/entries/form/:id' => 'entries#form'
   end
 end
